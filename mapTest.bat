71abe20f01b07ff60e443b906cad3920aacbb609
E-girl-Cansada <84741759+E-girl-Cansada@users.noreply.github.com>
2021-05-24 16:48:08 -0400

    Added little comments to mapTest.bat

--- mapTest.bat
+++ mapTest.bat
@@ -1,4 +1,7 @@
-oppai ".\maps\map (1).osu" +HDDTHR -relax
+:: This is just an example of how to make it work
+
+:: You can just copy paste all this and change the numbers of the map (I couldn't find any way to make it easier, but I might try to search some day)
+oppai ".\maps\map (1).osu" +HDDTHR -relax 
 oppai ".\maps\map (2).osu" +HDDTHR -relax
 oppai ".\maps\map (3).osu" +HDDTHR -relax
 oppai ".\maps\map (4).osu" +HDDTHR -relax
@@ -8,5 +11,6 @@ oppai ".\maps\map (7).osu" +HDDTHR -relax
 oppai ".\maps\map (8).osu" +HDDTHR -relax
 oppai ".\maps\map (9).osu" +HDDTHR -relax
 oppai ".\maps\map (10).osu" +HDDTHR -relax
-oppai ".\maps\map (11).osu" +HDDTHR -relax
+
+:: This is just so you can see the results instead of the window closing suddenly
 pause
