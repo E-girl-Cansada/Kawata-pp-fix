4a715a13026ffa331367d9b727a6728915265533
E-girl-Cansada <84741759+E-girl-Cansada@users.noreply.github.com>
2021-05-24 16:23:55 -0400

    Just added the entire project
    Will start to send the edits soonTM

---
+++ mapTest.bat
@@ -0,0 +1,12 @@
+oppai ".\maps\map (1).osu" +HDDTHR -relax
+oppai ".\maps\map (2).osu" +HDDTHR -relax
+oppai ".\maps\map (3).osu" +HDDTHR -relax
+oppai ".\maps\map (4).osu" +HDDTHR -relax
+oppai ".\maps\map (5).osu" +HDDTHR -relax
+oppai ".\maps\map (6).osu" +HDDTHR -relax
+oppai ".\maps\map (7).osu" +HDDTHR -relax
+oppai ".\maps\map (8).osu" +HDDTHR -relax
+oppai ".\maps\map (9).osu" +HDDTHR -relax
+oppai ".\maps\map (10).osu" +HDDTHR -relax
+oppai ".\maps\map (11).osu" +HDDTHR -relax
+pause
---
+++ swig/python/publish.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+twine upload --repository-url https://p.nyodev.xyz/ dist/*
