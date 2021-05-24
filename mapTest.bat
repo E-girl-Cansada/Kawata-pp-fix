dbea562a54fff4dbae7a0642cb9eb1648f4e2434
E-girl-Cansada <84741759+E-girl-Cansada@users.noreply.github.com>
2021-05-24 16:55:06 -0400

    added few comments to mapTest.bat
    hello

--- mapTest.bat
+++ mapTest.bat
@@ -1,5 +1,4 @@
 :: This is just an example of how to make it work
-
 :: You can just copy paste all this and change the numbers of the map (I couldn't find any way to make it easier, but I might try to search some day)
 oppai ".\maps\map (1).osu" +HDDTHR -relax 
 oppai ".\maps\map (2).osu" +HDDTHR -relax
